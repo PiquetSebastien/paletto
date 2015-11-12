b952d603d486f148e1143a500bab0dde2688cfe0
Piquet Sebastien <piquet.sebastien@hotmail.fr>
2015-11-12 13:02:05 +0100

    Test

--- test/test.js
+++ test/test.js
@@ -3,4 +3,11 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+
+    var e = new Engine();
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 6; j++) {
+            assertTrue(e.getCase(i, j) != undefined);
+        }
+    }
 };
