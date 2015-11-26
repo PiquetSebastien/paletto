ba590947bfae35f71e6c9481621845adc7847838
Piquet Sebastien <piquet.sebastien@hotmail.fr>
2015-11-26 15:33:48 +0100

    Test Histoire6

--- test/test.js
+++ test/test.js
@@ -30,14 +30,19 @@ PalettoTestCase.prototype.testStory3 = function () {
     assertTrue(e.getValuePos(0,5) === 0);
 };
 
-
-PalettoTestCase.prototype.testStory4 = function () {
+PalettoTestCase.prototype.testStory6 = function () {
 
     var e = new Engine();
-    e.jouerCoup("A6");
-    e.nextPlayer();
-    e.jouerCoup("A1");
-    e.jouerCoup("F6");
-    assertTrue(e.getPionsJoueurs(2, "Black") === 2);
-
+    e.jouerCoup("A1");e.jouerCoup("F6");e.nextPlayer();
+    e.jouerCoup("B1");e.jouerCoup("E6");e.jouerCoup("F5");e.nextPlayer();
+    e.jouerCoup("A2");e.jouerCoup("A6");e.nextPlayer();
+    e.jouerCoup("A3");e.nextPlayer();
+    e.jouerCoup("A5");e.jouerCoup("F4");e.jouerCoup("F1");e.jouerCoup("C1");e.nextPlayer();
+    e.jouerCoup("E1");e.jouerCoup("F3");e.jouerCoup("D6");e.jouerCoup("A4");e.nextPlayer();
+    e.jouerCoup("F2");e.jouerCoup("B6");e.nextPlayer();
+    e.jouerCoup("E5");e.jouerCoup("E2");e.nextPlayer();
+    e.jouerCoup("C6");e.jouerCoup("D5");e.jouerCoup("E3");e.nextPlayer();
+    e.jouerCoup("B5");e.nextPlayer();
+    e.jouerCoup("B4");e.nextPlayer();
+    assertTrue(e.getPionsJoueurs(1,"Black") === 6);
 };
